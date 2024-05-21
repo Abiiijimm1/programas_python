88e9bf8e4d032a27ba1ba8979ca89012e8f96744
Abigail Jiménez Rojas <82452636+Abiiijimm1@users.noreply.github.com>
2024-05-20 20:26:37 -0600

    Create ejercicio13.py

---
+++ ejercicio13.py
@@ -0,0 +1,16 @@
+'''
+Ejercicio 13
+Obtener el cuadrado de la suma de dos listas de números utilizando map
+'''
+
+def suma_cuadrados (a,b):
+    return (a + b) ** 2
+lista1 = [1,2,3,4]
+lista2 = [5,6,7,8]
+
+resultado = list (map (suma_cuadrados, lista1,lista2))
+
+print (lista1)
+print (lista2)
+
+print (resultado)
